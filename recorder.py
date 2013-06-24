106a0556cf7b647b6848e7f0f519f0115ca3165f
Jan Gosmann <jan@hyper-world.de>
2013-06-24 16:15:30 +0200

    Recorder for controls.

--- recorder.py
+++ recorder.py
@@ -1,3 +1,4 @@
+from qrsim.tcpclient import ctrl_signal_dimensions, UAVControls
 import numpy as np
 import tables
 
@@ -57,3 +58,55 @@ class TaskPlumeRecorder(GeneralRecorder):
 
     plume_measurements = property(lambda self: self._plume_measurements.read())
     rewards = property(lambda self: self._rewards.read())
+
+
+class ControlsRecorder(object):
+    class Controls(tables.IsDescription):
+        type = tables.EnumCol(
+            ctrl_signal_dimensions.keys(),
+            ctrl_signal_dimensions.keys()[0], tables.Int8Atom())
+        dt = tables.FloatCol()
+        uav = tables.Int8Col()
+        U = tables.Float32Col(shape=max(ctrl_signal_dimensions.values()))
+
+    def __init__(self, fileh, client, expected_steps=None):
+        self.fileh = fileh
+        self.client = client
+        self.expected_steps = expected_steps
+
+    def init(self, task, realtime=False):
+        self.client.init(task, realtime)
+        self.controls = self.fileh.create_table(
+            self.fileh.root, 'controls', self.Controls,
+            title='Controls used for each step command.',
+            expectedrows=self.expected_steps)
+
+    def step(self, dt, controls):
+        row = self.controls.row
+        fillup = len(row['U']) - controls.U.shape[1]
+        for uav in xrange(controls.num_uavs):
+            row['type'] = self.Controls.columns['type'].enum[controls.type]
+            row['dt'] = dt
+            row['uav'] = uav
+            row['U'] = np.concatenate(
+                (controls.U[uav, :], np.repeat(0, fillup)))
+            row.append()
+        self.client.step(dt, controls)
+
+    def step_ctrl(self, dt, ctrl):
+        controls = UAVControls(len(ctrl), 'ctrl')
+        controls.U[:, :] = ctrl
+        self.step(dt, controls)
+
+    def step_vel(self, dt, vels):
+        controls = UAVControls(len(vels), 'vel')
+        controls.U[:, :] = vels
+        self.step(dt, controls)
+
+    def step_wp(self, dt, WPs):
+        controls = UAVControls(len(WPs), 'wp')
+        controls.U[:, :] = WPs
+        self.step(dt, controls)
+
+    def __getattr__(self, name):
+        return getattr(self.client, name)
--- test/test_recorder.py
+++ test/test_recorder.py
@@ -1,9 +1,9 @@
-from hamcrest import assert_that
+from hamcrest import assert_that, is_, only_contains
 from matchers import has_items_in_relative_order
 from mock import ANY, call, MagicMock
 from numpy.testing import assert_equal
-from qrsim.tcpclient import UAVState
-from recorder import GeneralRecorder, TaskPlumeRecorder
+from qrsim.tcpclient import ctrl_signal_dimensions, UAVControls, UAVState
+from recorder import ControlsRecorder, GeneralRecorder, TaskPlumeRecorder
 import numpy as np
 import tables
 import tempfile
@@ -64,3 +64,106 @@ class TestTaskPlumeRecorder(TestGeneralRecorder):
         assert_equal(
             self.recorder.rewards,
             [-np.inf] + (steps - 1) * [self.client.get_reward.return_value])
+
+
+class TestControlsRecorder(object):
+    def setUp(self):
+        self.tmp_file = tempfile.NamedTemporaryFile()
+        self.fileh = tables.open_file(self.tmp_file.name, 'w')
+        self.client = MagicMock()
+        self.client.numUAVs = 2
+        self.recorder = ControlsRecorder(self.fileh, self.client)
+        self.recorder.init('Task', False)
+
+    def tearDown(self):
+        self.fileh.close()
+        self.tmp_file.close()
+
+    def test_init_of_client_is_called(self):
+        self.client.init.assert_called_once_with('Task', False)
+
+    def test_has_attrs_of_client(self):
+        assert_that(self.recorder.numUAVs, is_(self.client.numUAVs))
+        assert_that(self.recorder.timestep, is_(self.client.timestep))
+
+    def tests_forwards_unknown_method_calls_to_client(self):
+        self.recorder.unknown_method('some arg')
+        self.client.unknown_method.assert_called_once_with('some arg')
+
+    def check_control_recording_type_specific_step(self, type):
+        dt = 0.1
+        steps = 3
+        dim = ctrl_signal_dimensions[type]
+        for i in xrange(steps):
+            getattr(self.recorder, 'step_' + type)(
+                dt, [range(dim), range(dim, 2 * dim)])
+        self.fileh.flush()
+
+        controls = self.recorder.controls
+        assert_that(controls.cols.type, only_contains(
+            getattr(ControlsRecorder.Controls.columns['type'].enum, type)))
+        assert_that(controls.cols.dt[:], only_contains(0.1))
+        assert_equal(
+            [x['U'][:dim] for x in controls.where('uav == 0')],
+            np.tile(range(dim), (steps, 1)))
+        assert_equal(
+            [x['U'][:dim] for x in controls.where('uav == 1')],
+            np.tile(range(dim, 2 * dim), (steps, 1)))
+
+    def check_control_recording_of_step(self, type):
+        input_controls = UAVControls(self.recorder.numUAVs, type)
+        for i in xrange(self.recorder.numUAVs):
+            input_controls.U[i, :] = i
+        dt = 0.1
+        steps = 3
+
+        for i in xrange(steps):
+            self.recorder.step(dt, input_controls)
+        self.fileh.flush()
+
+        dim = ctrl_signal_dimensions[type]
+        controls = self.recorder.controls
+        assert_that(controls.cols.type, only_contains(
+            getattr(ControlsRecorder.Controls.columns['type'].enum, type)))
+        assert_that(controls.cols.dt[:], only_contains(0.1))
+        for i in xrange(self.recorder.numUAVs):
+            assert_equal(
+                [x['U'][:dim] for x in controls.where('uav == %i' % i)],
+                np.tile(dim * [i], (steps, 1)))
+
+    def check_client_step_called(self, type):
+        input_controls = UAVControls(self.recorder.numUAVs, type)
+        for i in xrange(self.recorder.numUAVs):
+            input_controls.U[i, :] = i
+        dt = 0.1
+
+        self.recorder.step(dt, input_controls)
+        self.client.step.assert_called_once_with(dt, input_controls)
+
+    def check_client_step_type_specific_called(self, type):
+        input_controls = UAVControls(self.recorder.numUAVs, type)
+        for i in xrange(self.recorder.numUAVs):
+            input_controls.U[i, :] = i
+        dt = 0.1
+
+        getattr(self.recorder, 'step_' + type)(dt, input_controls.U)
+
+        class MatchInputControls(object):
+            def __init__(self, obj):
+                self.obj = obj
+
+            def __eq__(self, other):
+                if self.obj.type != other.type:
+                    return False
+                assert_equal(self.obj.U, other.U)
+                return True
+
+        self.client.step.assert_called_once_with(
+            dt, MatchInputControls(input_controls))
+
+    def test_records_controls(self):
+        for type in ctrl_signal_dimensions.keys():
+            yield self.check_control_recording_type_specific_step, type
+            yield self.check_control_recording_of_step, type
+            yield self.check_client_step_called, type
+            yield self.check_client_step_type_specific_called, type
