4c3b77ce50d8bec77da041555ad0930f4dbc4d27
Jan Gosmann <jan@hyper-world.de>
2013-07-08 14:51:10 +0200

    Fix predict_on_volume grid transposition.

--- prediction.py
+++ prediction.py
@@ -2,8 +2,10 @@ import numpy as np
 
 
 def predict_on_volume(predictor, area, grid_resolution):
-    x, y, z = np.meshgrid(*[np.linspace(*dim, num=res) for dim, res in zip(
-        area, grid_resolution)])
+    ogrid = [np.linspace(*dim, num=res) for dim, res in zip(
+        area, grid_resolution)]
+    x, y, z = (np.rollaxis(m, 1) for m in np.meshgrid(*ogrid))
+
     pred, mse = predictor.predict(
         np.column_stack((x.flat, y.flat, z.flat)), eval_MSE=True)
 
