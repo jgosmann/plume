1501eb7aa7c40924a00ca615193efc8d979d4da6
Jan Gosmann <jan@hyper-world.de>
2013-07-26 11:47:45 +0200

    Refactor plume.py command line arguments.

--- plume.py
+++ plume.py
@@ -53,19 +53,28 @@ class Controller(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('conf', nargs=1, type=str)
-    parser.add_argument('output', nargs=1, type=str)
-    parser.add_argument('ip', nargs='?', type=str, default=['127.0.0.1'])
-    parser.add_argument('port', nargs='?', type=int, default=[10000])
+    parser.add_argument(
+        '-c', '--config', nargs=1, type=str, help='Configuration to load.')
+    parser.add_argument(
+        '-o', '--output', nargs=1, type=str, default=['plume.h5'],
+        help='Output file name.')
+    parser.add_argument(
+        '-H', '--host', nargs=1, type=str, default=['127.0.0.1'],
+        help='Host running QRSim.')
+    parser.add_argument(
+        '-P', '--port', nargs=1, type=int, default=[10000],
+        help='Port on which QRSim instance is listening.')
+    parser.add_argument(
+        'output_dir', nargs=1, type=str, help='Output directory.')
     args = parser.parse_args()
 
     logging.basicConfig()
     logger.setLevel(logging.INFO)
 
-    conf = load_config(args.conf[0])
+    conf = load_config(args.config[0])
     predictor = conf['predictor']
 
-    output_filename = os.path.join(args.output[0], 'plume.h5')
+    output_filename = os.path.join(args.output_dir[0], args.output[0])
 
     with tables.open_file(output_filename, 'w') as fileh:
         fileh.set_node_attr('/', 'conf', conf)
@@ -73,7 +82,7 @@ if __name__ == '__main__':
 
         with TaskPlumeClient() as client:
             client = ControlsRecorder(fileh, client, num_steps)
-            client.connect_to(args.ip[0], args.port[0])
+            client.connect_to(args.host[0], args.port[0])
             recorder = TaskPlumeRecorder(fileh, client, predictor, num_steps)
 
             controller = Controller(client, conf['behavior'])
