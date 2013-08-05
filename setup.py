354db95b4729ef7d0044d2061b1c979f015d2827
Jan Gosmann <jan@hyper-world.de>
2013-08-05 17:15:50 +0200

    Reorganizing as python package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+# FIXME add dependencies (nose)
+setup(
+    name='plume',
+    version='0.1',
+    description='Algorithms and visualization code for my master thesis.',
+    author='Jan Gosmann',
+    author_email='jan@hyper-world.de',
+    packages=find_packages())
