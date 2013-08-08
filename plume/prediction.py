Fixing sign error in derivatives.

plume/prediction.py
@@ -82,7 +82,7 @@ class RBFKernel(object):
         res = self.variance * np.exp(-0.5 * d)
         if eval_derivative:
             s = np.rollaxis(np.atleast_3d(x1) - np.atleast_3d(x2).T, 1, 3)
-            der = self.variance / self.lengthscale * s * \
+            der = -self.variance / self.lengthscale * s * \
                 np.exp(-0.5 * d)[:, :, None]
             return res, der
         else:
@@ -133,7 +133,7 @@ class OnlineGP(object):
             pred_derivative = np.einsum(
                 'ijk,jl->ilk', K_new_vs_old_derivative, svs)
             if eval_MSE:
-                mse_derivative = 2 * np.einsum(
+                mse_derivative = -2 * np.einsum(
                     'ijk,ji->ik', K_new_vs_old_derivative, mse_svs)
                 return pred, pred_derivative, mse, mse_derivative
             else:

plume/test/test_prediction.py
@@ -20,10 +20,10 @@ class TestRBFKernel(object):
         x1 = np.array([[1, 1, 1], [1, 2, 1]])
         x2 = np.array([[1, 2, 3], [4, 2, 1]])
         expected = np.array([
-            [[0.0, -0.00200828, -0.00401657],
-             [-5.80780750e-06, -1.93593583e-06, 0.0]],
-            [[0.0, 0.0, -0.016108],
-             [-2.32915823e-05, 0.00000000e+00, 0.00000000e+00]]])
+            [[0.0, 0.00200828, 0.00401657],
+             [5.80780750e-06, 1.93593583e-06, 0.0]],
+            [[0.0, 0.0, 0.016108],
+             [2.32915823e-05, 0.00000000e+00, 0.00000000e+00]]])
         unused, actual = RBFKernel(lengthscale=0.6, variance=0.75)(
             x1, x2, eval_derivative=True)
         assert_almost_equal(actual, expected)
@@ -81,7 +81,7 @@ class TestOnlineGP(object):
         self.gp.fit(x, y)
 
         x_star = np.array([[-3, 1]]).T
-        expected = np.array([[[-0.85538797]], [[1.30833924]]])
+        expected = np.array([[[0.85538797]], [[-1.30833924]]])
         unused, actual = self.gp.predict(x_star, eval_derivatives=True)
         assert_almost_equal(actual, expected)
 