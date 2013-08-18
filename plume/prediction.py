Fix MSE calculation for non RBF kernels.

plume/prediction.py
@@ -133,8 +133,7 @@ class OnlineGP(object):
         pred = np.dot(K_new_vs_old, svs)
         if eval_MSE:
             mse_svs = np.dot(self.K_inv.data, K_new_vs_old.T)
-            # FIXME formula works only for rbf kernel etc
-            mse = self.noise_var + self.kernel.variance - np.einsum(
+            mse = self.noise_var + self.kernel.diag(x, x) - np.einsum(
                 'ij,ji->i', K_new_vs_old, mse_svs)
 
         if eval_derivatives: