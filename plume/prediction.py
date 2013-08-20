aa48bc9dcae39f95b250537900cd01928db7958c
Jan Gosmann <jan@hyper-world.de>
2013-08-20 14:41:28 +0200

    Numerical stability of mse estimation.

--- plume/prediction.py
+++ plume/prediction.py
@@ -144,8 +144,10 @@ class OnlineGP(object):
         pred = np.dot(K_new_vs_old, svs)
         if eval_MSE:
             mse_svs = np.dot(K_inv, K_new_vs_old.T)
-            mse = self.noise_var + self.kernel.diag(x, x) - np.einsum(
-                'ij,ji->i', K_new_vs_old, mse_svs)
+            mse = np.maximum(
+                self.noise_var,
+                self.noise_var + self.kernel.diag(x, x) - np.einsum(
+                    'ij,ji->i', K_new_vs_old, mse_svs))
 
         if eval_derivatives:
             pred_derivative = np.einsum(
