75fd28397ac6bca0eeb0fb5e91720043c8deb8c8
Jan Gosmann <jan@hyper-world.de>
2013-09-12 15:01:27 +0200

    Fix typo.

--- plume/prediction.py
+++ plume/prediction.py
@@ -244,7 +244,7 @@ class OnlineGP(object):
         kernel_derivative = np.array([
             0.5 * np.sum(np.einsum('ij,ji->i', grad_weighting, param_deriv))
             for param_deriv in self.kernel.param_derivatives(
-                self.x_train.data, self.x_train_data)])
+                self.x_train.data, self.x_train.data)])
 
         return -log_likelihood, -kernel_derivative
 
