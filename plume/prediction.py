Fix ExponentialKernel derivative (factor of 2).

plume/prediction.py
@@ -74,7 +74,7 @@ class ExponentialKernel(object):
         res = self.variance * np.exp(-d / self.lengthscale)
         if eval_derivative:
             s = x1[:, None, :] - x2[None, :, :]
-            der = -2.0 / d[:, :, None] / self.lengthscale * s * res[:, :, None]
+            der = -s / d[:, :, None] / self.lengthscale * res[:, :, None]
             return res, der
         else:
             return res

plume/test/test_prediction.py
@@ -74,10 +74,10 @@ class TestExponentialKernel(object):
         x1 = np.array([[1, 1, 1], [1, 2, 1]])
         x2 = np.array([[1, 2, 3], [4, 2, 1]])
         expected = np.array([
-            [[0.0, 0.02691132, 0.05382264],
-             [0.0121935, 0.0040645, 0.0]],
-            [[0.0, 0.0, 0.089184983368130993],
-             [0.016844867497713668, 0.0, 0.0]]])
+            [[0.0, 0.01345566, 0.02691132],
+             [0.00609675, 0.00203225, -0.0]],
+            [[0.0, 0.0, 0.0445924916840655],
+             [0.008422433748856834, 0.0, 0.0]]])
         unused, actual = ExponentialKernel(lengthscale=0.6, variance=0.75)(
             x1, x2, eval_derivative=True)
         assert_almost_equal(actual, expected)