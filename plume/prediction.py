Fix AnisotropicExponentialKernel param derivative.

plume/prediction.py
@@ -101,6 +101,7 @@ class ExponentialKernel(object):
 
 class AnisotropicExponentialKernel(object):
     def __init__(self, lengthscale_mat, variance=1.0):
+        lengthscale_mat = np.asarray(lengthscale_mat)
         assert lengthscale_mat.shape[0] == lengthscale_mat.shape[1]
         self.num_dim = lengthscale_mat.shape[0]
         self.params = np.concatenate((
@@ -146,16 +147,19 @@ class AnisotropicExponentialKernel(object):
         variance_deriv = np.exp(-pd)
 
         proj_deriv_component = -np.einsum(
-            'ik,jl->ijkl', self.projection, self.L_inv.T)
-        proj_deriv = np.einsum(
-            'ak,ijkl->ijal', self.projection,
-            proj_deriv_component + np.transpose(
-                proj_deriv_component, (0, 1, 3, 2)))
-        x1_proj_sq_norm = np.einsum('ak,ijkl,bl->ijab', x1, proj_deriv, x1)
-        x2_proj_sq_norm = np.einsum('ak,ijkl,bl->ijab', x2, proj_deriv, x2)
+            'ik,jl->ijkl', self.projection, self.L_inv)
+        proj_deriv = proj_deriv_component + np.transpose(
+            proj_deriv_component, (0, 1, 3, 2))
 
-        lengthscale_deriv = -self.variance * variance_deriv / pd * (
-            x1_proj_sq_norm - x2_proj_sq_norm)
+        d = x1[:, None, :] - x2[None, :, :]
+        d_proj = np.einsum(
+            'abk,kl,ijlm,abm->ijab', d, self.projection, proj_deriv, d)
+
+        zero_dist_indices = np.abs(pd) < np.finfo(pd.dtype).eps
+        pd[zero_dist_indices] = 1.0  # prevent warnings
+        lengthscale_deriv = -self.variance * \
+            (variance_deriv / pd)[None, None, :, :] * d_proj
+        lengthscale_deriv[:, :, zero_dist_indices] = 0.0
         return np.concatenate((
             lengthscale_deriv[np.tril_indices(self.num_dim)],
             variance_deriv[None, :, :]))