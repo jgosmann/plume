69633695d135de9bc5c26ac176b069cfe3a07a5c
Jan Gosmann <jan@hyper-world.de>
2013-09-19 10:43:28 +0200

    Fix navigation.

--- plume/behaviors.py
+++ plume/behaviors.py
@@ -21,36 +21,17 @@ class VelocityTowardsWaypointController(object):
 
         controls = UAVControls(len(noisy_states), 'vel')
         for uav in xrange(len(noisy_states)):
-            phi = noisy_states[uav].phi
-            theta = noisy_states[uav].theta
-            psi = noisy_states[uav].psi
-
-            X = np.matrix(
-                [[1, 0, 0],
-                 [0, np.cos(phi), -np.sin(phi)],
-                 [0, np.sin(phi), np.cos(phi)]])
-            Y = np.matrix(
-                [[np.cos(theta), 0, np.sin(theta)],
-                 [0, 1, 0],
-                 [-np.sin(theta), 0, np.cos(theta)]])
-            Z = np.matrix(
-                [[np.cos(psi), -np.sin(psi), 0],
-                 [np.sin(psi), np.cos(psi), 0],
-                 [0, 0, 1]])
-
-            world_v = 0.15 * np.array(
-                [self.maxv, self.maxv, self.max_climb]) * \
+            v = 0.05 * np.array([self.maxv, self.maxv, self.max_climb]) * \
                 (targets[uav] - noisy_states[uav].position)
-            if norm(world_v[:2]) > self.maxv:
-                world_v[:2] *= self.maxv / norm(world_v[:2])
-            world_v[2] = np.clip(world_v[2], -self.max_climb, self.max_climb)
+            if norm(v[:2]) > self.maxv:
+                v[:2] *= self.maxv / norm(v[:2])
+            v[2] = np.clip(v[2], -self.max_climb, self.max_climb)
 
             outside_low = noisy_states[uav].position < self.area[:, 0]
             outside_high = noisy_states[uav].position > self.area[:, 1]
-            world_v[outside_low] = self.max_speeds[outside_low]
-            world_v[outside_high] = -self.max_speeds[outside_high]
-
-            controls.U[uav, :] = np.dot(Z * Y * X, world_v)
+            v[outside_low] = self.max_speeds[outside_low]
+            v[outside_high] = -self.max_speeds[outside_high]
+            controls.U[uav, :] = v
 
         return controls
 
