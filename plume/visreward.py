51fb7f46ce8428eefc741240be5de8402fdc83fa
Jan Gosmann <jan@hyper-world.de>
2013-10-10 16:37:12 +0200

    Plot RMSE and WRMSE.

--- plume/visreward.py
+++ plume/visreward.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     with tables.openFile(args.filename[0], 'r') as data:
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
-        ax.plot(data.root.rewards)
-        ax.plot(-100 * data.root.rmse.read(), label='rmse')
-        ax.plot(-100 * data.root.wrmse.read(), label='wrmse')
+        #ax.plot(data.root.rewards)
+        ax.plot(data.root.rmse.read(), label='rmse')
+        ax.plot(data.root.wrmse.read(), label='wrmse')
         plt.legend()
         plt.ioff()
         plt.show()
