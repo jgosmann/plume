df8112bfa519ccb589107c1fa1a67aca17164027
Jan Gosmann <jan@hyper-world.de>
2013-10-27 10:25:49 +0100

    Data synchronization script.

---
+++ syncdata.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rsync -rltvz wisteria.ki.tu-berlin.de:~/master/plume/.fridge/* ~/Documents/programming/uni/master/plume/.fridge/
+rsync -rltvz wisteria.ki.tu-berlin.de:~/master/plume/Data/ ~/Documents/programming/uni/master/plume/Data/
