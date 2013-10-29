bd8018c4bf04782eb257fbe97b0ea9c4effa3336
Jan Gosmann <jan@hyper-world.de>
2013-10-29 16:09:53 +0100

    Fix measurement noise handling in try_kernels.

--- plume/try_kernels.py
+++ plume/try_kernels.py
@@ -73,8 +73,8 @@ class KernelTester(object):
         test_x = x[self.conf['train_size']:]
         test_y = y[self.conf['train_size']:]
 
-        if self.conf['measurement_noise_var'] > 1e-6:
-            train_y += np.sqrt(self.conf['noise_var']) * rnd.randn(
+        if self.conf['measurement_noise_var'] > 0:
+            train_y += np.sqrt(self.conf['measurement_noise_var']) * rnd.randn(
                 len(train_y))
 
         for measure in self.measures:
