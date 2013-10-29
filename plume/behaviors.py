3208cc4aca87f5c37c41f41ec04febbff2915a52
Jan Gosmann <jan@hyper-world.de>
2013-10-29 16:08:39 +0100

    Fix PDUCB auto MSE scaling.

--- plume/behaviors.py
+++ plume/behaviors.py
@@ -270,9 +270,8 @@ class PDUCB(DUCBBased):
                 max_val = self.predictor.y_bv.max()
             else:
                 max_val = self.predictor.y_train.data.max()
-            return self.kappa * np.log(
-                max_val + self.epsilon) - np.log(
-                self.epsilon)
+            return self.kappa * (np.log(
+                max_val + self.epsilon) - np.log(self.epsilon))
         else:
             return self.kappa * self.mse_scaling
 
