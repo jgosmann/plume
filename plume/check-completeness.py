193215b2f8ef41ac46137c8ddc494b22b010c9b2
Jan Gosmann <jan@hyper-world.de>
2013-11-12 14:38:01 +0100

    Generate tspec for missing sims.

--- plume/check-completeness.py
+++ plume/check-completeness.py
@@ -5,6 +5,13 @@ import itertools
 import os.path
 
 
+def missing_trials(directory, paramstr, num_trials):
+    for i in range(num_trials):
+        filename = '{}.{}.h5'.format(paramstr, i)
+        if not os.path.exists(os.path.join(directory, filename)):
+            yield i
+
+
 def checkfiles_for_paramstr(directory, paramstr, num_trials):
     for i in range(num_trials):
         filename = '{}.{}.h5'.format(paramstr, i)
@@ -12,9 +19,35 @@ def checkfiles_for_paramstr(directory, paramstr, num_trials):
             print filename
 
 
+def print_tspec_block(paramstr, behavior, kappa, gamma, scaling, trial):
+    if behavior == 'DUCB':
+        behavior_stmt = 'acq_fn = """(\'behaviors\', \'DUCB\', (), dict(' \
+            'kappa=kappa, scaling=scaling, gamma=gamma))"""'
+    elif behavior == 'PDUCB':
+        behavior_stmt = 'acq_fn = """(\'behaviors\', \'PDUCB\', (), dict(' \
+            'kappa=kappa, scaling=scaling, gamma=gamma, epsilon=1e-30))"""'
+    elif behavior == 'GO':
+        behavior_stmt = 'acq_fn = """(\'behaviors\', \'GO\', (), dict(' \
+            'gamma=gamma))"""'
+    else:
+        raise NotImplementedError()
+
+    print '''[{blockname}]
+    {behavior_stmt}
+    kappa = {k}
+    gamma = {g}
+    scaling = {s}
+    repeat = {r}
+'''.format(
+        blockname=paramstr, behavior_stmt=behavior_stmt, k=kappa, g=gamma,
+        s=scaling, r=trial)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('dir', nargs=1, type=str, help='Directory to check')
+    parser.add_argument(
+        '--tspec', action='store_true', help='Print tspec block')
     args = parser.parse_args()
 
     behaviors = ['DUCB', 'PDUCB']
@@ -28,9 +61,19 @@ if __name__ == '__main__':
         for s in scaling[b]:
             paramstr = '_{b}_scaling={s} kappa={k} gamma={g}'.format(
                 b=b, s=s, k=k, g=g)
-            checkfiles_for_paramstr(args.dir[0], paramstr, num_trials)
+            if args.tspec:
+                for repeat in missing_trials(
+                        args.dir[0], paramstr, num_trials):
+                    print_tspec_block(paramstr, b, k, g, s, repeat)
+            else:
+                checkfiles_for_paramstr(args.dir[0], paramstr, num_trials)
 
     # Check GO files
     for g in gammas:
         paramstr = '_GO_gamma={g}'.format(g=g)
-        checkfiles_for_paramstr(args.dir[0], paramstr, num_trials)
+        if args.tspec:
+            for repeat in missing_trials(
+                    args.dir[0], paramstr, num_trials):
+                print_tspec_block(paramstr, b, k, g, s, repeat)
+        else:
+            checkfiles_for_paramstr(args.dir[0], paramstr, num_trials)
