f36e5aa91a7cdeb70076d18a1467e7193959ec8a
Jan Gosmann <jan@hyper-world.de>
2013-11-12 14:16:59 +0100

    Check for correct scaling in completeness check.

--- plume/check-completeness.py
+++ plume/check-completeness.py
@@ -21,13 +21,14 @@ if __name__ == '__main__':
     gammas = ['0', '-1e-9', '-1e-8', '-1e-7', '-1e-6', '-1e-5', '-1e-4',
               '-1e-3', '-1e-2']
     kappas = ['0.1', '0.5', '0.75', '1', '1.25', '1.5', '2']
-    scaling = ["'auto'", '1']
+    scaling = {'DUCB': ["'auto'", '1'], 'PDUCB': ["'auto'", '70']}
     num_trials = 20
 
-    for b, s, k, g in itertools.product(behaviors, scaling, kappas, gammas):
-        paramstr = '_{b}_scaling={s} kappa={k} gamma={g}'.format(
-            b=b, s=s, k=k, g=g)
-        checkfiles_for_paramstr(args.dir[0], paramstr, num_trials)
+    for b, k, g in itertools.product(behaviors, kappas, gammas):
+        for s in scaling[b]:
+            paramstr = '_{b}_scaling={s} kappa={k} gamma={g}'.format(
+                b=b, s=s, k=k, g=g)
+            checkfiles_for_paramstr(args.dir[0], paramstr, num_trials)
 
     # Check GO files
     for g in gammas:
