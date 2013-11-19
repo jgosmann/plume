b58876cb4f15dc231cf42ee322ed5287fa8e5fac
Jan Gosmann <jan@hyper-world.de>
2013-11-19 11:06:57 +0100

    Also sync noise sim data.

--- syncdata.sh
+++ syncdata.sh
@@ -2,3 +2,4 @@
 
 rsync -rltvz wisteria.ki.tu-berlin.de:~/master/plume/.fridge/* ~/Documents/programming/uni/master/plume/.fridge/
 rsync -rltvz wisteria.ki.tu-berlin.de:~/master/plume/Data/ ~/Documents/programming/uni/master/plume/Data/
+rsync -rltvz wisteria.ki.tu-berlin.de:~/master/noise/plume/Data/ ~/Documents/programming/uni/master/plume/Data/
